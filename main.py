ecc3b713f4d9f5611a3a626036f4a70474fbb6bc
LaAliss <alice.rigotto@student.h-farm.com>
2019-11-27 13:46:10 -0500

    used the sys module

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 #! /usr/bin/env python3
 
-'''We are importing check module from NewPackage'''
+'''In the main.py we are importing the module check from mypackage'''
 
-from NewPackage import checks
+from NewPackage import capitals
 
-checks.capitals.check_capital("Germany")
-checks.capitals.check_capital("Honduras")
-checks.capitals.check_state("Rome")
-checks.capitals.check_state("Tokyo")
+import sys 
+
+if __name__ == __main__:
+	capitals.check_capital(sys.argv[1])
+else:
+	print("Please type a valide capital")
+
+
+checks.check_capital("Germany")
+checks.check_capital("Honduras")
+checks.check_state("Rome")
+checks.check_state("Tokyo")
+
 
 
