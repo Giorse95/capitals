d891e938710cfc578da2cd66a948de00fedc7765
LaAliss <alice.rigotto@student.h-farm.com>
2019-12-03 13:19:27 -0500

    added sys.argv

--- main.py
+++ main.py
@@ -4,18 +4,19 @@
 
 from NewPackage import capitals
 
-import sys 
+import sys
+
 
 if __name__ == "__main__":
+    if len(sys.argv) == 2:
 	if sys.argv[1] == 'check':
 		capitals.check_capital("Germany")
 		capitals.check_capital("Honduras")
 		capitals.check_state("Rome")
 		capitals.check_state("Tokyo")
 	else:
-		capitals.check_state(sys.argv[1])
-
-
+		capitals.check_state(sys.arg[1])
+		capitals.check_capital(sys.arg[1])
 
 
 
